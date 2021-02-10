77b97d31cd66c2bcb2123980f6b4948d55c0d70b
Mukhin <mukhinaa89@yandex.ru>
2021-02-10 17:14:35 +0300

    1.7. Конструктор [#427698]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        this.desc = text;
+    }
+
+    public static void main(String[] args) {
+        String qwe = "Fixed";
+        Fix fix = new Fix(qwe);
+    }
+
+}
